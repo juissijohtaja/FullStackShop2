9cd9a87e5df4fced6a0fb2b85e4580e907a025d1
JuissiJohtaja <juha.koivu@outlook.com>
2020-01-01 14:37:15 +0200

    Semantic UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Form, Button, Table, Menu, Link } from 'semantic-ui-react'
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
+  Form
+} from 'semantic-ui-react'
 import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 import './App.css'
 import fire from './fire'
 import ProductForm from './components/ProductForm'
 import Products from './components/Products'
+import HomepageLayout from './components/HomepageLayout'
+
 
 //import Notes from './components/Notes'
 import { initializeNotes } from './reducers/noteReducer'
@@ -32,41 +50,46 @@ const App = (props) => {
   }
 
   return (
-    <Container>
-      <Menu inverted color='red' >
-        <Menu.Item link>
-          <a onClick={() => setPage('products')}>Products</a>
-        </Menu.Item>
-        <Menu.Item link>
-          <a onClick={() => setPage('productform')}>Add Product</a>
-        </Menu.Item>
-      </Menu>
-      <h2>Old School Shop</h2>
+    <div>
+      <HomepageLayout show={page === 'homepagelayout'} />
+      <Container>
+        <Menu inverted color='red'>
+          <Menu.Item link>
+            <a onClick={() => setPage('products')}>Products</a>
+          </Menu.Item>
+          <Menu.Item link>
+            <a onClick={() => setPage('productform')}>Add Product</a>
+          </Menu.Item>
+          <Menu.Item link>
+            <a onClick={() => setPage('homepagelayout')}>HomepageLayout</a>
+          </Menu.Item>
+        </Menu>
+        <h2>Old School Shop</h2>
 
-      <Products show={page === 'products'} />
-      <ProductForm show={page === 'productform'}  />
+        <ProductForm show={page === 'productform'}  />
 
-      <div>
-        <h3>Messages Redux</h3>
-        <ul>
-          {props.messages.map(message => <li key={message.id}>{message.text}</li>)}
-        </ul>
-      </div>
-
-      <Form onSubmit={AddMessage}>
         <div>
-          <Form.Field>
-            <label>Add message</label>
-            <input
-              type="text"
-              value={newMessage}
-              onChange={({ target }) => setNewMessage(target.value)}
-            />
-          </Form.Field>
+          <h3>Messages Redux</h3>
+          <ul>
+            {props.messages.map(message => <li key={message.id}>{message.text}</li>)}
+          </ul>
         </div>
-        <Button type='submit'>Save</Button>
-      </Form>
-    </Container>
+
+        <Form onSubmit={AddMessage}>
+          <div>
+            <Form.Field>
+              <label>Add message</label>
+              <input
+                type="text"
+                value={newMessage}
+                onChange={({ target }) => setNewMessage(target.value)}
+              />
+            </Form.Field>
+          </div>
+          <Button type='submit'>Save</Button>
+        </Form>
+      </Container>
+    </div>
   )
 }
 
